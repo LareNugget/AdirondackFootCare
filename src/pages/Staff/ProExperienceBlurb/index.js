420aa4f8e2be4d66ae47d7e3e2911351e5be662a
LareNugget <larenugget33@gmail.com>
2022-07-27 16:39:43 -0400

    removed proexperience blurb on staff page

--- src/pages/Staff/ProExperienceBlurb/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import * as SC from "./styles";
-
-function ProExperienceBlurb() {
-  return <div>ProExperienceBlurb</div>;
-}
-
-export default ProExperienceBlurb;
--- src/pages/Staff/index.js
+++ src/pages/Staff/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import * as SC from "./styles";
 import PageLabel from "./PageLabel";
-import ProExperienceBlurb from "./ProExperienceBlurb";
 import Doctors from "./Doctors";
 import OfficeStaff from "./OfficeStaff";
 
@@ -9,7 +8,6 @@ function Staff() {
   return (
     <SC.Container>
       <PageLabel />
-      <ProExperienceBlurb />
       <Doctors />
       <OfficeStaff />
     </SC.Container>
