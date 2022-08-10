c8f7b489741f73493667fa3ab8d399cce6f0ce6e
LareNugget <larenugget33@gmail.com>
2022-08-10 17:24:42 -0400

    changes insurance picture file types

--- src/pages/Home/Insurances/index.js
+++ src/pages/Home/Insurances/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import * as SC from "./styles";
-import Aetna from "../../../images/insurance/aetna.png";
+import { Link } from "react-router-dom";
+import Aetna from "../../../images/insurance/aetna.jpg";
 import AARP from "../../../images/insurance/aarp.jpg";
-import Blueshield from "../../../images/insurance/blueshield.png";
+import Blueshield from "../../../images/insurance/blueshield.jpg";
 import CDPHP from "../../../images/insurance/cdphp.jpg";
 import Empire from "../../../images/insurance/empire.jpg";
 import Medicare from "../../../images/insurance/medicare.jpg";
-import MVP from "../../../images/insurance/mvp.png";
-import Progressive from "../../../images/insurance/progressive.png";
-import Tricare from "../../../images/insurance/tricare.png";
+import MVP from "../../../images/insurance/mvp.jpg";
+import Progressive from "../../../images/insurance/progressive.jpg";
+import Tricare from "../../../images/insurance/tricare.jpg";
 
 function Insurances() {
   return (
@@ -28,7 +29,9 @@ function Insurances() {
       </SC.CardContainer>
 
       <SC.ButtonContainer>
-        <button>View All Providers</button>
+        <Link to="/insurance">
+          <button>View All Providers</button>
+        </Link>
       </SC.ButtonContainer>
     </SC.Container>
   );
