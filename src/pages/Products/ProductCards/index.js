11270e73f571b4b2139e6003f0b22e337880e055
LareNugget <larenugget33@gmail.com>
2022-08-10 17:27:05 -0400

    added products page components

---
+++ src/pages/Products/ProductCards/index.js
@@ -0,0 +1,260 @@
+import React from "react";
+import * as SC from "./styles";
+import Alaplex from "../../../images/products/alaplex.jpg";
+import Bare40 from "../../../images/products/bare40.jpg";
+import Betadine from "../../../images/products/betadine.jpg";
+import Colig10 from "../../../images/products/colig10.jpg";
+import FastFreeze from "../../../images/products/fastfreeze.jpg";
+import Formula3 from "../../../images/products/formula3.jpg";
+import Formula7 from "../../../images/products/formula7.jpg";
+import Fortinia from "../../../images/products/fortinia.jpg";
+import FungiFoam from "../../../images/products/fungifoam.jpg";
+import GelSocks from "../../../images/products/gelsocks.jpg";
+import GreenSoap from "../../../images/products/greensoap.jpg";
+import HempNesic from "../../../images/products/hempnesic.jpg";
+import Kamea20 from "../../../images/products/kamea20.jpg";
+import KameaG from "../../../images/products/kameag.jpg";
+import Luminail from "../../../images/products/luminail.jpg";
+import Orthonesic from "../../../images/products/orthonesic.jpg";
+import Probetic20 from "../../../images/products/probetic20.jpg";
+import ToePack from "../../../images/products/toepack.jpg";
+import Vircin from "../../../images/products/vircin.jpg";
+
+function ProductCards() {
+  return (
+    <SC.Container>
+      <SC.Header>Products</SC.Header>
+      <SC.CardContainer>
+        <SC.ProductCard>
+          <img src={Alaplex} alt="Alaplex" />
+          <SC.CardText>
+            <div>Alaplex</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={Bare40} alt="Bare40" />
+          <SC.CardText>
+            <div>Bare 40</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={Betadine} alt="Betadine" />
+          <SC.CardText>
+            <div>Betadine</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={Colig10} alt="Colig10" />
+          <SC.CardText>
+            <div>Colig 10</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={FastFreeze} alt="FastFreeze" />
+          <SC.CardText>
+            <div>Fast Freeze</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={Formula3} alt="Formula3" />
+          <SC.CardText>
+            <div>Formula 3</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={Formula7} alt="Formula7" />
+          <SC.CardText>
+            <div>Formula 7</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={Fortinia} alt="Fortinia" />
+          <SC.CardText>
+            <div>Fortinia</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={FungiFoam} alt="FungiFoam" />
+          <SC.CardText>
+            <div>FungiFoam</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={GelSocks} alt="Gel Socks" />
+          <SC.CardText>
+            <div>Gel Socks</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={GreenSoap} alt="Green Soap" />
+          <SC.CardText>
+            <div>Green Soap</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={HempNesic} alt="Hempnesic" />
+          <SC.CardText>
+            <div>Hempnesic</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={Kamea20} alt="Kamea 20" />
+          <SC.CardText>
+            <div>Kamea 20</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={KameaG} alt="Kamea G" />
+          <SC.CardText>
+            <div>Kamea G</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={Luminail} alt="Luminail" />
+          <SC.CardText>
+            <div>Luminail</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={Orthonesic} alt="Orthonesic" />
+          <SC.CardText>
+            <div>Orthonesic</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={Probetic20} alt="Probetic 20" />
+          <SC.CardText>
+            <div>Probetic 20</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={ToePack} alt="Toe Pack" />
+          <SC.CardText>
+            <div>Toe Pack</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+
+        <SC.ProductCard>
+          <img src={Vircin} alt="Vircin" />
+          <SC.CardText>
+            <div>Vircin</div>
+            <p>
+              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
+              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
+              pain lol Relieves foot pain lol Relieves foot pain lol
+            </p>
+          </SC.CardText>
+        </SC.ProductCard>
+      </SC.CardContainer>
+    </SC.Container>
+  );
+}
+
+export default ProductCards;
