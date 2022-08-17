92c1dabb6c37f42bc7624a5d326efa28cb1c3b8e
LareNugget <larenugget33@gmail.com>
2022-08-17 18:57:04 -0400

    added navButtonPress to NavButtons

--- src/components/Navbar/NavButtons/index.js
+++ src/components/Navbar/NavButtons/index.js
@@ -2,22 +2,22 @@ import React from "react";
 import * as SC from "./styles";
 import { Link } from "react-router-dom";
 
-function NavButtons() {
+function NavButtons({ navButtonPress }) {
   return (
     <SC.ButtonContainer>
-      <Link to="/staff">
+      <Link to="/staff" onClick={navButtonPress}>
         <button>Staff</button>
       </Link>
 
-      <Link to="/services">
+      <Link to="/services" onClick={navButtonPress}>
         <button>Services</button>
       </Link>
 
-      <Link to="/products">
+      <Link to="/products" onClick={navButtonPress}>
         <button>Products</button>
       </Link>
 
-      <Link to="/insurance">
+      <Link to="/insurance" onClick={navButtonPress}>
         <button>Insurance</button>
       </Link>
     </SC.ButtonContainer>
