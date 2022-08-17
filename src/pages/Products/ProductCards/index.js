af85af01a2ca4d5946d4a1801ecf5e2a9c978e64
LareNugget <larenugget33@gmail.com>
2022-08-17 01:40:49 -0400

    added product descriptions

--- src/pages/Products/ProductCards/index.js
+++ src/pages/Products/ProductCards/index.js
@@ -8,7 +8,7 @@ import FastFreeze from "../../../images/products/fastfreeze.jpg";
 import Formula3 from "../../../images/products/formula3.jpg";
 import Formula7 from "../../../images/products/formula7.jpg";
 import Fortinia from "../../../images/products/fortinia.jpg";
-import FungiFoam from "../../../images/products/fungifoam.jpg";
+import Glycylic from "../../../images/products/glycylic.jpg";
 import GelSocks from "../../../images/products/gelsocks.jpg";
 import GreenSoap from "../../../images/products/greensoap.jpg";
 import HempNesic from "../../../images/products/hempnesic.jpg";
@@ -30,9 +30,12 @@ function ProductCards() {
           <SC.CardText>
             <div>Alaplex</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Alaplex is a supplement that helps with burning, tingling,
+              numbness, and shooting pains in the lower extremity. It is a
+              combination of vitamins and alphalipoic acid. It also contains
+              Benfotiamine, which is an enhanced Thiamine
+              {"(commonly known as Vitamin B1."} Improvement can be seen in as
+              little as 12 weeks!
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -42,9 +45,9 @@ function ProductCards() {
           <SC.CardText>
             <div>Bare 40</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Maximum strength 40% Urea gel. It is an advanced treatment for
+              severely cracked and fissured heels and skin. Can be applied
+              morning and night. No prescription required!
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -54,9 +57,12 @@ function ProductCards() {
           <SC.CardText>
             <div>Betadine</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Betadine is an antimicrobial antiseptic that can be used for all
+              types of wound care applications. It can be used in conjunction
+              with other antimicrobials, or by itself.{" "}
+              <span style={{ color: "yellow" }}>
+                Betadine is an iodine containing product!
+              </span>
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -66,9 +72,9 @@ function ProductCards() {
           <SC.CardText>
             <div>Colig 10</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Colig 10 is a supplement used for collagen, ligament, and tendon
+              support. It is commonly used to treat soft tissue injuries, such
+              as sprains and tendonitis.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -78,9 +84,8 @@ function ProductCards() {
           <SC.CardText>
             <div>Fast Freeze</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Fast Freeze is a quick relief, pain soothing gel that is excellent
+              for sprains and strains, as well as minor foot and joint pain.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -90,9 +95,9 @@ function ProductCards() {
           <SC.CardText>
             <div>Formula 3</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Formula 3 is the #1 prescribed antifungal solution for treatment
+              of fungal toenails {"(onychomycosis)"}. It is dispensed in the
+              form of a lotion.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -102,9 +107,9 @@ function ProductCards() {
           <SC.CardText>
             <div>Formula 7</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Formula 7 is an alternative to Formula 3, designed for topical use
+              on fungal toenails. It is dispensed in the form of a liquid
+              solution.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -114,21 +119,19 @@ function ProductCards() {
           <SC.CardText>
             <div>Fortinia</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Fortinia is a 1.5% phenol solution. It is an excellent antiseptic
+              for interdigital tinea {"(fungus)"}.
             </p>
           </SC.CardText>
         </SC.ProductCard>
 
         <SC.ProductCard>
-          <img src={FungiFoam} alt="FungiFoam" />
+          <img src={Glycylic} alt="Glycylic" />
           <SC.CardText>
-            <div>FungiFoam</div>
+            <div>Glycylic</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Glycylic is a maximum strength 17% salicylic acid treatment for
+              warts.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -138,9 +141,9 @@ function ProductCards() {
           <SC.CardText>
             <div>Gel Socks</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Gel socks are specially designed socks which allow patients to
+              apply ointments and creams to their feet without messy cleanup.
+              This is done by containing the ointments and creams on your feet.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -150,9 +153,9 @@ function ProductCards() {
           <SC.CardText>
             <div>Green Soap</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Green soap is an excellent product to exfoliate and reduce callous
+              and hard skin on the feet. It is a great way to hydrate, clean
+              your feet prior to applying a moisturizer or cream.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -162,9 +165,9 @@ function ProductCards() {
           <SC.CardText>
             <div>Hempnesic</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Hempnesic is a maximum pain relief, over the counter CBD
+              containing product providing natural pain relief. Derived from
+              hemp seed oil, camphor, menthol, and capsaicin.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -174,9 +177,8 @@ function ProductCards() {
           <SC.CardText>
             <div>Kamea 20</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Kamea 20 is a 20% Urea based cream that is an excellent
+              moisturizer. It can be purchased without a prescription.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -186,9 +188,9 @@ function ProductCards() {
           <SC.CardText>
             <div>Kamea G</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Kamea G is a 20% Urea based cream that includes glycolic acid,
+              which is an exfoliator. This product is an excellent choice if you
+              have severely fissured or calloused skin.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -198,9 +200,8 @@ function ProductCards() {
           <SC.CardText>
             <div>Luminail</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Luminail is a nail restoration product that contains eucalyptus,
+              tea tree oil, as well as other natural moisturizing agents.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -210,9 +211,8 @@ function ProductCards() {
           <SC.CardText>
             <div>Orthonesic</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Orthonesic is a quick relief, pain soothing gel that is excellent
+              for sprains and strains, as well as minor foot and joint pain.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -222,9 +222,8 @@ function ProductCards() {
           <SC.CardText>
             <div>Probetic 20</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Probetic 20 is a moisturizing foot cream that is used to restore
+              normal skin texture.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -234,9 +233,10 @@ function ProductCards() {
           <SC.CardText>
             <div>Toe Pack</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Toe packs are dispensed and prescribed following ingrown toenail
+              procedures, as well as minor surgeries of the toes and feet. This
+              pack will eliminate the costly purchases of dressings at
+              pharmacies.
             </p>
           </SC.CardText>
         </SC.ProductCard>
@@ -246,9 +246,10 @@ function ProductCards() {
           <SC.CardText>
             <div>Vircin</div>
             <p>
-              Relieves foot pain lol Relieves foot pain lol Relieves foot pain
-              lol Relieves foot pain lol Relieves foot pain lol Relieves foot
-              pain lol Relieves foot pain lol Relieves foot pain lol
+              Vircin is an antiviral agent that is used in the treatment of
+              plantar warts. It is applied in the evening, left on overnight,
+              and removed in the morning. Excellent for uncomplicated types of
+              verruca/warts.
             </p>
           </SC.CardText>
         </SC.ProductCard>
