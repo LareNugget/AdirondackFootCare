eff8e9ca355cbc74cd19c61a23dfc74b5fd3aeb1
LareNugget <larenugget33@gmail.com>
2022-08-17 01:41:05 -0400

    re-arranged insurances on insurance page

--- src/pages/Insurance/InsuranceCards/index.js
+++ src/pages/Insurance/InsuranceCards/index.js
@@ -9,12 +9,48 @@ import Medicare from "../../../images/insurance/medicare.jpg";
 import MVP from "../../../images/insurance/mvp.jpg";
 import Progressive from "../../../images/insurance/progressive.jpg";
 import TriCare from "../../../images/insurance/tricare.jpg";
+import NYSHIP from "../../../images/insurance/nyship.jpg";
 
 function InsuranceCards() {
   return (
     <SC.Container>
       <SC.Header>Providers</SC.Header>
       <SC.CardContainer>
+        <SC.InsuranceCard>
+          <img src={CDPHP} alt="CDPHP" />
+          <SC.CardText>
+            <div>CDPHP</div>
+          </SC.CardText>
+        </SC.InsuranceCard>
+
+        <SC.InsuranceCard>
+          <img src={NYSHIP} alt="NYS Government Employees Plan" />
+          <SC.CardText>
+            <div>NYSHIP</div>
+          </SC.CardText>
+        </SC.InsuranceCard>
+
+        <SC.InsuranceCard>
+          <img src={Blueshield} alt="Blueshield" />
+          <SC.CardText>
+            <div>Blueshield</div>
+          </SC.CardText>
+        </SC.InsuranceCard>
+
+        <SC.InsuranceCard>
+          <img src={MVP} alt="MVP" />
+          <SC.CardText>
+            <div>MVP</div>
+          </SC.CardText>
+        </SC.InsuranceCard>
+
+        <SC.InsuranceCard>
+          <img src={Medicare} alt="Medicare" />
+          <SC.CardText>
+            <div>Medicare</div>
+          </SC.CardText>
+        </SC.InsuranceCard>
+
         <SC.InsuranceCard>
           <img src={AARP} alt="AARP" />
           <SC.CardText>
@@ -30,37 +66,9 @@ function InsuranceCards() {
         </SC.InsuranceCard>
 
         <SC.InsuranceCard>
-          <img src={Blueshield} alt="Blueshield" />
-          <SC.CardText>
-            <div>Blueshield</div>
-          </SC.CardText>
-        </SC.InsuranceCard>
-
-        <SC.InsuranceCard>
-          <img src={CDPHP} alt="CDPHP" />
-          <SC.CardText>
-            <div>CDPHP</div>
-          </SC.CardText>
-        </SC.InsuranceCard>
-
-        <SC.InsuranceCard>
           <img src={Empire} alt="Empire" />
           <SC.CardText>
             <div>Empire</div>
-          </SC.CardText>
-        </SC.InsuranceCard>
-
-        <SC.InsuranceCard>
-          <img src={Medicare} alt="Medicare" />
-          <SC.CardText>
-            <div>Medicare</div>
-          </SC.CardText>
-        </SC.InsuranceCard>
-
-        <SC.InsuranceCard>
-          <img src={MVP} alt="MVP" />
-          <SC.CardText>
-            <div>MVP</div>
           </SC.CardText>
         </SC.InsuranceCard>
 
